9a275746d9a7f546c75ecca1a7adc092c55c0a5b
Nicofisi <m.lomiak@gmail.com>
2022-04-10 21:30:21 +0200

    Finish level 3 of game

--- guessing/main.go
+++ guessing/main.go
@@ -6,55 +6,75 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
-const MaxValue = 100
+const MaxValue = 10
 
 func getRandomNumber() int {
 	return rand.Intn(MaxValue + 1)
 }
 
+func readLine(scanner *bufio.Scanner) string {
+	scanner.Scan()
+	input := scanner.Text()
+
+	if err := scanner.Err(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, "error | reading standard input:", err)
+	}
+
+	return input
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
-
-	fmt.Println("welcome | Hello human! You will be guessing numbers.")
-	fmt.Print("next unknown 0 1000 | Proceed with a number between 0 and 1000: ")
-
-	target := getRandomNumber()
-	tries := 0
 
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for true {
-		tries += 1
+		fmt.Println("welcome | Hello human! You will be guessing numbers.")
+		maxValueStr := strconv.Itoa(MaxValue)
+		fmt.Print("next unknown " + maxValueStr + " | Proceed with a number between 0 and " + maxValueStr + ": ")
 
-		scanner.Scan()
-		input := scanner.Text()
+		target := getRandomNumber()
+		tries := 0
 
-		if err := scanner.Err(); err != nil {
-			_, _ = fmt.Fprintln(os.Stderr, "error | reading standard input:", err)
+		for true {
+			tries += 1
+
+			input := readLine(scanner)
+
+			if input == "koniec" {
+				os.Exit(0)
+			}
+
+			// string to int
+			inputInt, err := strconv.Atoi(input)
+			if err != nil {
+				_, _ = fmt.Fprintln(os.Stderr, "error | your input is not 'koniec' nor a valid integer: ", err)
+			}
+
+			if inputInt > target {
+				fmt.Print("next lower 0 " + maxValueStr + " | The number you guessed is too high! Try again: ")
+			} else if inputInt < target {
+				fmt.Print("next higher 0 " + maxValueStr + " | The number you guessed is too low! Try again: ")
+			} else {
+				triesStr := strconv.Itoa(tries)
+				fmt.Println("success " + triesStr + " | You got it in " + triesStr + " tries!")
+				break
+			}
 		}
 
-		if input == "koniec" {
-			os.Exit(0)
+		fmt.Print("again | Play again? [Y/n] ")
+
+		input := readLine(scanner)
+
+		if input == "" || strings.ToLower(input) == "y" || strings.ToLower(input) == "yes" {
+			continue
 		}
 
-		// string to int
-		inputInt, err := strconv.Atoi(input)
-		if err != nil {
-			_, _ = fmt.Fprintln(os.Stderr, "error | your input is not 'koniec' nor a valid integer: ", err)
-		}
-
-		if inputInt > target {
-			fmt.Print("next lower 0 1000 | The number you guessed is too high! Try again: ")
-		} else if inputInt < target {
-			fmt.Print("next higher 0 1000 | The number you guessed is too low! Try again: ")
-		} else {
-			triesStr := strconv.Itoa(tries)
-			fmt.Println("success " + triesStr + " | You got it in " + triesStr + " tries!")
-			break
-		}
+		os.Exit(0)
 	}
 
 	dt := time.Now().AddDate(0, 0, 15)
