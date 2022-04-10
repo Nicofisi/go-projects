2132804984a77eef0fb7a111f00ff9da7b502b3b
Nicofisi <m.lomiak@gmail.com>
2022-04-10 20:16:22 +0200

    Finish level 1 of game

--- guessing/main.go
+++ guessing/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
 func getRandomNumber() int {
 	min := 0
-	max := 50
+	max := 1000
 	random := rand.Intn(max-min+1) + min
 	return random
 }
@@ -16,12 +17,31 @@ func getRandomNumber() int {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	rd := getRandomNumber()
+	fmt.Println("welcome | Hello human! You will be guessing numbers.")
+	fmt.Print("next unknown 0 1000 | Proceed with a number between 0 and 1000: ")
 
-	fmt.Println(rd)
+	target := getRandomNumber()
+	tries := 0
+
+	for true {
+		tries += 1
+		var input int
+
+		if _, err := fmt.Scanf("%d\n", &input); err != nil {
+		}
+		if input > target {
+			fmt.Print("next lower 0 1000 | The number you guessed is too high! Try again: ")
+		} else if input < target {
+			fmt.Print("next higher 0 1000 | The number you guessed is too low! Try again: ")
+		} else {
+			triesStr := strconv.Itoa(tries)
+			fmt.Println("success " + triesStr + " | You got it in " + triesStr + " tries!")
+		}
+	}
 
 	dt := time.Now().AddDate(0, 0, 15)
-	fmt.Println(dt.Format("01-02-2006"))
+	dt.Format("2006-01-02")
+	fmt.Println()
 
 	//json, _ := json.Marshal()
 }
